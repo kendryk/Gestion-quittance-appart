568c69e362d6a0bd6e546f64f85740478a5e0f41
Cedric MACE <cedric.mace@open-groupe.com>
2025-05-28 16:30:04 +0200

    feat(config, quittance-service): ajout de la configuration initiale des services
    - Implémentation du service métier `quittance-service` avec Spring Boot.
    - Ajout du service technique `config` pour centraliser la configuration avec Spring Cloud Config Server.
    - Intégration des dépendances nécessaires dans le POM parent pour Spring Boot, Swagger, MapStruct, et gestion des multi-modules.
    - Définition des fichiers de configuration `application.yml` et `quittance-service.yml` pour les paramètres techniques et métiers.

---
+++ service-metier/quittance-service/src/main/java/fr/quittance/service/QuittanceServiceApplication.java
@@ -0,0 +1,12 @@
+package fr.quittance.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuittanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuittanceServiceApplication.class, args);
+    }
+}
